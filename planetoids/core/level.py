53c49ff1f7b6cda15fbc4140d9577f658fefa636
Christopher Greening <chris@christophergreening.com>
2025-03-20 23:46:21 -0400

    ADD: improved the spacing for level

--- planetoids/core/level.py
+++ planetoids/core/level.py
@@ -24,8 +24,18 @@ class Level:
         return self.level
 
     def draw(self, screen):
-        """Display current level number."""
-        x_offset = {"minimum": 120, "medium": 170, "maximum": 320}.get(self.settings.get("pixelation"), 200)
-        y_offset = {"minimum": 30, "medium": 40, "maximum": 55}.get(self.settings.get("pixelation"), 200)
+        """Display current level number in the bottom-right, right-aligned."""
+
+        # Render text
         text = self.font.render(f"Level: {self.level}", True, config.WHITE)
-        screen.blit(text, (config.WIDTH - x_offset, config.HEIGHT - y_offset))  # Display bottom-right
+        text_rect = text.get_rect()
+
+        # Use offset to account for font size / padding
+        x_offset = {"minimum": 30, "medium": 40, "maximum": 60}.get(self.settings.get("pixelation"), 40)
+        y_offset = {"minimum": 10, "medium": 15, "maximum": 20}.get(self.settings.get("pixelation"), 40)
+
+        # Position the bottom-right corner of the text
+        text_rect.bottomright = (config.WIDTH - x_offset, config.HEIGHT - y_offset)
+
+        screen.blit(text, text_rect)
+
